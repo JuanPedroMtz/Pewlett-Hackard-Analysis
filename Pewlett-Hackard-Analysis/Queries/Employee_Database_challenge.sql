45e197cf06b189c2d40279b19e4d86385d7338e6
Juan Pedro Martínez <juanpedromartinezv@gmail.com>
2022-11-14 19:52:06 -0700

    Add files via upload

--- Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -8,10 +8,11 @@ SELECT e.emp_no,
 INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t
-ON (e.emp_no = t.emp_no::INT)
+ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
 --AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 ORDER BY e.emp_no ASC;
+SELECT * FROM retirement_titles;
 
 
 -- Use Dictinct with Orderby to remove duplicate rows
@@ -24,7 +25,6 @@ INTO unique_titles
 FROM retirement_titles as rt
 WHERE (rt.to_date = '9999-01-01')
 ORDER BY rt.emp_no, rt.to_date DESC;
-
 
 --Retiring Titles
 SELECT COUNT (title), title
